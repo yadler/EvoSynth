583a38172f790da36e4b6734419617618357eb0b
Yves Adler <yves.adler@googlemail.com>
2009-12-31 15:19:05 +0100

    fixed some problems

--- examples/partition.rb
+++ examples/partition.rb
@@ -26,24 +26,39 @@ require 'evosynth'
 module Partitionproblem
 
 	class PartitionIndividual
-		include EvoSynth::MinimizingIndividual
+		include Comparable
 
-		attr_accessor :partition_border
+		attr_accessor :partition_a, :partition_b
 
-		def initialize(genome)
-			@genome = genome
-			@partition_border = rand(genome.size - 1)
-			@genome.changed = true
+		def initialize(partition_a, partition_b)
+			@partition_a = partition_a;
+			@partition_b = partition_b;
 		end
 
-		def calculate_fitness
-			sum_a = @genome[0..@partition_border].inject(0) { |sum, n| n.nil? ? sum : sum + n }
-			sum_b = @genome[@partition_border + 1..@genome.size].inject(0) { |sum, n| n.nil? ? sum : sum + n }
-			(sum_a - sum_b).abs
+		def fitness
+			if @partition_a.changed || @partition_b.changed
+				sum_a = @partition_a.inject(0) { |sum, n| sum + n }
+				sum_b = @partition_b.inject(0) { |sum, n| sum + n }
+				@fitness = (sum_a - sum_b).abs
+			end
+
+			@fitness
+		end
+
+		def deep_clone
+			my_clone = self.clone
+			my_clone.partition_a = self.partition_a.clone
+			my_clone.partition_b = self.partition_b.clone
+			my_clone
+		end
+
+		def <=>(anOther)
+			cmp = fitness <=> anOther.fitness
+			-1 * cmp
 		end
 
 		def to_s
-			"fitness = #{fitness}, partition_a = [#{@genome[0..@partition_border]}], partition_b = [#{@genome[@partition_border + 1..@genome.size]}]"
+			"fitness = #{fitness}, partition_a = #{@partition_a}, partition_b = #{@partition_a}"
 		end
 
 	end
@@ -51,19 +66,13 @@ module Partitionproblem
 	class PartitionMutation
 
 		def mutate(individual)
-			# FIXME this mutation is absolute bullshit - maybe I should go for a 2 array genome
 			mutated = individual.deep_clone
-			genome = mutated.genome
+			part_a, part_b = mutated.partition_a, mutated.partition_b
 
 			if rand(2) > 0
-				rand_index = rand(genome.size - mutated.partition_border)
-				foo = genome.delete_at(mutated.partition_border + rand_index)
-				genome.insert(0, foo)
-				mutated.partition_border += 1
+				part_a << part_b.delete_at(rand(part_b.size)) unless mutated.partition_b.empty?
 			else
-				rand_index = rand(mutated.partition_border)
-				genome.insert(mutated.partition_border, genome.delete_at(rand_index))
-				mutated.partition_border -= 1
+				part_b << part_a.delete_at(rand(part_a.size)) unless mutated.partition_a.empty?
 			end
 
 			mutated
@@ -72,6 +81,7 @@ module Partitionproblem
 		def to_s
 			"custom made mutation for the partition problem"
 		end
+
 	end
 
 	module Testdata
@@ -107,8 +117,15 @@ module Partitionproblem
 		end
 	end
 
-	def Partitionproblem.get_genome_from(problem)
-		genome = EvoSynth::Genome.new(problem)
+	def Partitionproblem.get_new_individual_from(problem)
+		border = rand(problem.size)
+		part_a, part_b = EvoSynth::Genome.new, EvoSynth::Genome.new
+
+		problem.size.times do
+			|index| index < border ? part_a << problem[index] : part_b << problem[index]
+		end
+
+		Partitionproblem::PartitionIndividual.new(part_a, part_b)
 	end
 
 	def Partitionproblem.run_algorithm(algorithm_class, population, generations)
@@ -118,11 +135,8 @@ module Partitionproblem
 		algorithm.mutation = PartitionMutation.new
 		algorithm.recombination = EvoSynth::Recombinations::Identity.new if defined? algorithm.recombination
 
-		# algorithm.selection = EvoSynth::Selections::SelectBest.new if defined? algorithm.selection
+		# The tournament selection seems to be the best performing
 		algorithm.selection = EvoSynth::Selections::TournamentSelection.new(3) if defined? algorithm.selection
-		# algorithm.selection = EvoSynth::Selections::StochasticUniversalSampling.new if defined? algorithm.selection
-		# algorithm.selection = EvoSynth::Selections::NStageTournamentSelection.new(3) if defined? algorithm.selection
-		# algorithm.selection = EvoSynth::Selections::FitnessProportionalSelection.new if defined? algorithm.selection
 
 		result = algorithm.run_until_generations_reached(generations)
 		puts algorithm
@@ -130,17 +144,14 @@ module Partitionproblem
 		puts "\tbest individual: #{result.best}"
 		puts "\tworst individual: #{result.worst}"
 	end
-	def evolve
-
-	end
 
 end
 
-POPULATION_SIZE = 1
-GENERATIONS = 100000
+POPULATION_SIZE = 10
+GENERATIONS = 1000
 
 problem = Partitionproblem::Testdata.gen_layer_set
-population = EvoSynth::Population.new(POPULATION_SIZE) { Partitionproblem::PartitionIndividual.new(Partitionproblem.get_genome_from(problem)) }
+population = EvoSynth::Population.new(POPULATION_SIZE) { Partitionproblem.get_new_individual_from(problem) }
 
 puts "Starting with the following population:"
 puts "\tbest individual: #{population.best}"
@@ -149,9 +160,4 @@ puts
 
 Partitionproblem.run_algorithm(EvoSynth::Algorithms::PopulationHillclimber, population.deep_clone, GENERATIONS)
 Partitionproblem.run_algorithm(EvoSynth::Algorithms::SteadyStateGA, population.deep_clone, GENERATIONS)
-#Partitionproblem.run_algorithm(EvoSynth::Algorithms::GeneticAlgorithm, population.deep_clone, GENERATIONS)
-
-puts
-puts "The startpopulation should not have changed:"
-puts "\tbest individual: #{population.best}"
-puts "\tworst individual: #{population.worst}"
+Partitionproblem.run_algorithm(EvoSynth::Algorithms::GeneticAlgorithm, population.deep_clone, GENERATIONS)
