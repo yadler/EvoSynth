c5ff190195e8dc92c1672264d52354b521cefb35
Yves Adler <yves.adler@googlemail.com>
2010-01-12 17:30:11 +0100

    added ordered recombination

---
+++ lib/evosynth/operators/recombinations/ordered_recombination.rb
@@ -0,0 +1,90 @@
+#	Copyright (c) 2009, 2010 Yves Adler <yves.adler@googlemail.com>
+#
+#	Permission is hereby granted, free of charge, to any person
+#	obtaining a copy of this software and associated documentation
+#	files (the "Software"), to deal in the Software without
+#	restriction, including without limitation the rights to use,
+#	copy, modify, merge, publish, distribute, sublicense, and/or sell
+#	copies of the Software, and to permit persons to whom the
+#	Software is furnished to do so, subject to the following
+#	conditions:
+#
+#	The above copyright notice and this permission notice shall be
+#	included in all copies or substantial portions of the Software.
+#
+#	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+#	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+#	OTHER DEALINGS IN THE SOFTWARE.
+
+
+require 'set'
+
+
+module EvoSynth
+	module Recombinations
+
+		# ORDNUNGSREKOMBINATION (Weicker page 29)
+		# 
+		# TODO: this can only recombine permutations? (see partially mapped crossover)
+		#       -> at least it only works on genomes without duplicate genes!
+
+		class OrderedRecombination
+
+			def recombine(parent_one, parent_two)
+				shorter = EvoSynth::Recombinations.individual_with_shorter_genome(parent_one, parent_two)
+
+				child_one = recombine_to_one(parent_one, parent_two, shorter)
+				child_two = recombine_to_one(parent_two, parent_one, shorter)
+
+				[child_one, child_two]
+			end
+
+			def to_s
+				"mapping recombination"
+			end
+
+			private
+
+			def recombine_to_one(parent_one, parent_two, shorter)
+				child = parent_one.deep_clone
+
+				rand_index = rand(shorter.genome.size)
+				used_genes = child.genome[0..rand_index].to_set
+
+				insert_index = rand_index + 1
+				parent_two.genome.each_with_index do |gene, index|
+					next if used_genes.include?(gene)
+
+					child.genome[insert_index] = gene
+					used_genes << gene
+					insert_index += 1
+					break if insert_index >= child.genome.size
+				end
+
+				child
+			end
+
+			def fill_range(range, mapping, used_genes, parent, child)
+				range.each do |index|
+					gene = parent.genome[index]
+					first_gene = gene
+
+					while used_genes.include?(gene) do
+						gene = mapping[gene]
+						raise "duplicate gene in genome. would cause infinite loop!" if gene == first_gene
+					end
+
+					child.genome[index] = gene
+					used_genes << gene
+				end
+			end
+
+		end
+
+	end
+end
---
+++ test/operators/recombinations/tc_ordered_recombination.rb
@@ -0,0 +1,76 @@
+#	Copyright (c) 2009, 2010 Yves Adler <yves.adler@googlemail.com>
+#
+#	Permission is hereby granted, free of charge, to any person
+#	obtaining a copy of this software and associated documentation
+#	files (the "Software"), to deal in the Software without
+#	restriction, including without limitation the rights to use,
+#	copy, modify, merge, publish, distribute, sublicense, and/or sell
+#	copies of the Software, and to permit persons to whom the
+#	Software is furnished to do so, subject to the following
+#	conditions:
+#
+#	The above copyright notice and this permission notice shall be
+#	included in all copies or substantial portions of the Software.
+#
+#	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+#	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+#	OTHER DEALINGS IN THE SOFTWARE.
+
+
+require 'shoulda'
+
+require 'evosynth'
+require 'test/test_util/test_helper'
+
+class OrderedRecombinationTest < Test::Unit::TestCase
+
+	context "a partially mapped crossover run on example genome (Weicker page 29)" do
+
+		setup do
+			@recombination = EvoSynth::Recombinations::OrderedRecombination.new
+
+			@individual_one = TestGenomeIndividual.new([1,4,8,6,5,7,2,3])
+			@individual_two = TestGenomeIndividual.new([1,2,3,4,8,5,6,7])
+		end
+
+		context "before the partially mapped crossover is executed" do
+
+			should "individual one should contain all numbers from 1 to 7" do
+				[1,2,3,4,5,6,7,8].each { |item| assert @individual_one.genome.include? item }
+			end
+
+			should "individual two should contain all numbers from 1 to 7" do
+				[1,2,3,4,5,6,7,8].each { |item| assert @individual_two.genome.include? item }
+			end
+
+		end
+
+		context "after the partially mapped crossover is executed" do
+
+			setup do
+				@child_one, @child_two = @recombination.recombine(@individual_one, @individual_two)
+			end
+
+			should "parent one should (still) contain all numbers from 1 to 7" do
+				[1,2,3,4,5,6,7,8].each { |item| assert @individual_one.genome.include? item }
+			end
+
+			should "parent two should (still) contain all numbers from 1 to 7" do
+				[1,2,3,4,5,6,7,8].each { |item| assert @individual_two.genome.include? item }
+			end
+
+			should "both children should (still) contain all numbers from 1 to 7" do
+				[1,2,3,4,5,6,7,8].each { |item| assert @child_one.genome.include? item }
+				[1,2,3,4,5,6,7,8].each { |item| assert @child_two.genome.include? item }
+			end
+
+		end
+
+	end
+
+end
