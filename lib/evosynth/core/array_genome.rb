67b1a1b1acda7687cc92940809092780ffe55736
Yves Adler <yves.adler@googlemail.com>
2010-01-15 16:44:24 +0100

    changed the metaprogramming code a bit

--- lib/evosynth/core/array_genome.rb
+++ lib/evosynth/core/array_genome.rb
@@ -22,9 +22,6 @@
 #	OTHER DEALINGS IN THE SOFTWARE.
 
 
-require 'delegate'
-
-
 module EvoSynth
 	module Core
 
@@ -35,39 +32,26 @@ module EvoSynth
 		class ArrayGenome < Array
 
 			# true if the genome has changed - has to be set to false manually
-
 			attr_accessor :changed
 
-			# see http://ruby-doc.org/doxygen/1.8.4/group__ruby__ary.html#ga9
-			# see rb_ary_store and rb_ary_modify
-
-			METHODS_THAT_CHANGE_ARRAY = ['[]=', 'delete', 'delete_at', 'collect!', 'map!', '<<', 'reject!', 'uniq!', 'unshift', 'shift',
-										 'sort!', 'pop', 'push', 'flatten!', 'reverse!', 'slice!', 'clear']
-
-			def initialize(*args)
-				@changed = true
-				super
-				overwrite_methods!
-			end
-
 			# Create a printable version of the genome
-
 			def to_s
 				self * ", "
 			end
 
-			# overwrites all methods of array that are listed in METHODS_THAT_CHANGE_ARRAY
+		end
 
-			def overwrite_methods!
-				METHODS_THAT_CHANGE_ARRAY.each do |method_name|
-					eval("def #{method_name}(*args)
-							  @changed = true
-							  super
-						  end")
-				end
-			end
-			private :overwrite_methods!
+		# see http://ruby-doc.org/doxygen/1.8.4/group__ruby__ary.html#ga9
+		# see rb_ary_store and rb_ary_modify
 
+		METHODS_THAT_CHANGE_ARRAY = ['initialize', '[]=', 'delete', 'delete_at', 'collect!', 'map!', '<<', 'reject!', 'uniq!', 'unshift',
+		                             'shift', 'sort!', 'pop', 'push', 'flatten!', 'reverse!', 'slice!', 'clear']
+
+		METHODS_THAT_CHANGE_ARRAY.each do |method_name|
+			ArrayGenome.class_eval("def #{method_name}(*args)
+									  @changed = true
+									  super
+								    end")
 		end
 
 	end
