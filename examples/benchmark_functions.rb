9d679dc320e91043bfb3290c65ef5c9bedd98b5b
Yves Adler <yves.adler@googlemail.com>
2010-01-20 14:54:56 +0100

    first running version of the CCGA-1

--- examples/benchmark_functions.rb
+++ examples/benchmark_functions.rb
@@ -30,7 +30,7 @@ module BenchmarkFunctionExample
 
 	VALUE_BITS = 16
 	DIMENSIONS = 5
-	GENERATIONS = 10000
+	GENERATIONS = 1000
 	POPULATION_SIZE = 25
 
 	class BenchmarkCalculator
@@ -43,35 +43,111 @@ module BenchmarkFunctionExample
 		end
 
 		def calculate_fitness(individual)
-			EvoSynth::BenchmarkFuntions.rastgrin(decode(individual))
+			EvoSynth::BenchmarkFuntions.sphere(decode(individual))
 		end
 
 	end
 
 
-	def BenchmarkFunctionExample.create_individual(genome_size)
-		individual = EvoSynth::Core::MinimizingIndividual.new
+	class CCGABenchmarkCalculator
+		include EvoSynth::Core::FitnessCalculator
+
+		def initialize(populations)
+			super()
+			@populations = populations
+		end
+
+		def decode_rand(individual)
+			values = (0..DIMENSIONS-1).to_a
+			values.each do |index|
+				if index == individual.population_index
+					values[index] = EvoSynth::Decoder.binary_to_real(individual.genome, -5.12, 5.12)
+				else
+					values[index] = EvoSynth::Decoder.binary_to_real(@populations[0][rand(@populations[0].size)].genome, -5.12, 5.12)
+				end
+			end
+
+			values
+		end
+
+		def decode(individual)
+			values = (0..DIMENSIONS-1).to_a
+			values.each do |index|
+				if index == individual.population_index
+					values[index] = EvoSynth::Decoder.binary_to_real(individual.genome, -5.12, 5.12)
+				else
+					values[index] = EvoSynth::Decoder.binary_to_real(@populations[0].best.genome, -5.12, 5.12)
+				end
+			end
+
+			values
+		end
+
+		def calculate_fitness(individual)
+			EvoSynth::BenchmarkFuntions.sphere(decode(individual))
+		end
+
+		def calculate_intitial_fitness(individual)
+			EvoSynth::BenchmarkFuntions.sphere(decode_rand(individual))
+		end
+
+	end
+
+	class CCGAIndividual < EvoSynth::Core::MinimizingIndividual
+		attr_accessor :population_index
+
+		def initialize(population_index)
+			@population_index = population_index
+			super()
+		end
+	end
+
+	def BenchmarkFunctionExample.create_individual(genome_size, index)
+		individual = CCGAIndividual.new(index)
 		individual.genome = EvoSynth::Core::ArrayGenome.new(genome_size)
 		individual.genome.map! { rand(2) > 0 ? true : false }
 		individual
 	end
 
 
-	profile = Struct.new(:individual, :mutation, :selection, :recombination, :population, :fitness_calculator).new
+	profile = Struct.new(:individual, :mutation, :selection, :recombination, :population, :populations, :fitness_calculator).new
 	profile.fitness_calculator = BenchmarkCalculator.new
 
 	profile.mutation = EvoSynth::Mutations::BinaryMutation.new(EvoSynth::Mutations::Functions::FLIP_BOOLEAN)
 	profile.selection = EvoSynth::Selections::FitnessProportionalSelection.new
 	profile.recombination = EvoSynth::Recombinations::KPointCrossover.new(2)
 
-	base_population = EvoSynth::Core::Population.new(POPULATION_SIZE) { BenchmarkFunctionExample.create_individual(VALUE_BITS*DIMENSIONS) }
-	profile.population = base_population.deep_clone
+	profile.population = EvoSynth::Core::Population.new(POPULATION_SIZE) { BenchmarkFunctionExample.create_individual(VALUE_BITS*DIMENSIONS, 0) }
 
-	algorithm = EvoSynth::Algorithms::ElitismGeneticAlgorithm.new(profile)
+
+	algorithm = EvoSynth::Algorithms::GeneticAlgorithm.new(profile)
+	algorithm.add_observer(EvoSynth::Util::ConsoleWriter.new(50, false))
+	result = algorithm.run_until_generations_reached(GENERATIONS)
+
+	# -------------------------------------------------------------------------------------- #
+
+	puts "# -------------------------------------------------------------------------------------- #"
+
+	profile.populations = []
+	DIMENSIONS.times do |dim|
+		profile.populations << EvoSynth::Core::Population.new(POPULATION_SIZE) { BenchmarkFunctionExample.create_individual(VALUE_BITS, dim) }
+	end
+
+	profile.fitness_calculator = CCGABenchmarkCalculator.new(profile.populations)
+
+	DIMENSIONS.times do |dim|
+		profile.populations[dim].each { |individual| profile.fitness_calculator.calculate_intitial_fitness(individual) }
+#		profile.populations[dim].each { |individual| puts "#{dim} == #{profile.fitness_calculator.decode_rand(individual).to_s}" }
+	end
+#	puts profile.populations
+
+	algorithm = EvoSynth::Algorithms::CCGA1.new(profile)
 	algorithm.add_observer(EvoSynth::Util::ConsoleWriter.new(100, false))
 	result = algorithm.run_until_generations_reached(GENERATIONS)
 
 	puts
 	puts result
+	puts profile.populations
 	puts profile.fitness_calculator
+
 end
