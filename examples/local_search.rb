a257f7ccb3691c3799d492443c12b111f470d53c
Yves Adler <yves.adler@googlemail.com>
2010-01-27 10:36:53 +0100

    added example exlusively for local search

---
+++ examples/local_search.rb
@@ -0,0 +1,86 @@
+#	Copyright (c) 2009, 2010 Yves Adler <yves.adler@googlemail.com>
+#
+#	Permission is hereby granted, free of charge, to any person
+#	obtaining a copy of this software and associated documentation
+#	files (the "Software"), to deal in the Software without
+#	restriction, including without limitation the rights to use,
+#	copy, modify, merge, publish, distribute, sublicense, and/or sell
+#	copies of the Software, and to permit persons to whom the
+#	Software is furnished to do so, subject to the following
+#	conditions:
+#
+#	The above copyright notice and this permission notice shall be
+#	included in all copies or substantial portions of the Software.
+#
+#	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+#	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+#	OTHER DEALINGS IN THE SOFTWARE.
+
+
+require 'evosynth'
+#require 'profile'
+
+
+module LocalSearch
+
+	VALUE_BITS = 16
+	DIMENSIONS = 5
+	GENERATIONS = 5000
+	GENOME_SIZE = VALUE_BITS * DIMENSIONS
+
+	class FitnessCalculator
+		include EvoSynth::Core::FitnessCalculator
+
+		def decode(individual)
+			values = []
+			DIMENSIONS.times { |dim| values << EvoSynth::Decoder.binary_to_real(individual.genome[dim * VALUE_BITS, VALUE_BITS], -5.12, 5.12) }
+			values
+		end
+
+		def calculate_fitness(individual)
+			EvoSynth::BenchmarkFuntions.sphere(decode(individual))
+		end
+
+	end
+
+	def LocalSearch.create_individual(genome_size)
+		individual = EvoSynth::Core::MaximizingIndividual.new
+		individual.genome = EvoSynth::Core::ArrayGenome.new(genome_size)
+		individual.genome.map! { rand(2) > 0 ? true : false }
+		individual
+	end
+
+	def LocalSearch.run_with(profile, individual)
+		puts "Local Search with #{profile.acceptance.to_s}\n\n"
+
+		profile.individual = individual.deep_clone
+		algorithm = EvoSynth::Algorithms::LocalSearch.new(profile)
+		algorithm.add_observer(EvoSynth::Util::ConsoleWriter.new(500, false));
+		algorithm.run_until_generations_reached(GENERATIONS)
+
+		puts "\n--> fitness after #{GENERATIONS} generations: #{profile.fitness_calculator.calculate_fitness(profile.individual)}\n\n"
+	end
+
+	profile = Struct.new(:individual, :mutation, :fitness_calculator, :acceptance).new
+	profile.mutation = EvoSynth::Mutations::BinaryMutation.new(EvoSynth::Mutations::Functions::FLIP_BOOLEAN)
+	profile.fitness_calculator = FitnessCalculator.new
+	individual = LocalSearch.create_individual(GENOME_SIZE)
+
+	profile.acceptance = EvoSynth::Algorithms::LocalSearch::HillclimberAcceptance.new
+	LocalSearch.run_with(profile, individual)
+
+	profile.acceptance = EvoSynth::Algorithms::LocalSearch::SimulatedAnnealingAcceptance.new
+	LocalSearch.run_with(profile, individual)
+
+	profile.acceptance = EvoSynth::Algorithms::LocalSearch::ThresholdAcceptance.new
+	LocalSearch.run_with(profile, individual)
+
+	profile.acceptance = EvoSynth::Algorithms::LocalSearch::GreatDelugeAcceptance.new
+	LocalSearch.run_with(profile, individual)
+
+end
