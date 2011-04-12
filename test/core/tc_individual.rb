bea290fafda4c585fd882a4a1e241e1a5cfc8085
René Köcher <rkoecher@googlemail.com>
2011-04-12 10:24:33 +0200

    fixed a bug in tc_individual.rb

--- test/core/tc_individual.rb
+++ test/core/tc_individual.rb
@@ -13,9 +13,10 @@ class IndividualTest < Test::Unit::TestCase
 			should "deep_clone makes a deep copy" do
 				my_clone = @individual.deep_clone
 				assert_not_equal my_clone.genome.object_id, @individual.genome.object_id
+				assert_equal my_clone.fitness, @individual.fitness
 				my_clone.fitness = 1.4
 				assert_not_equal my_clone.fitness, @individual.fitness
-				assert_nothing_raised {@individual.compare_fitness_values(1, 2)}
+				assert_nothing_raised {my_clone.compare_fitness_values(1, 2)}
 			end
 			
 		end
@@ -31,11 +32,8 @@ class IndividualTest < Test::Unit::TestCase
 				assert_equal my_clone.fitness, @individual.fitness
 				my_clone.fitness = 1.4
 				assert_not_equal my_clone.fitness, @individual.fitness
-				assert_nothing_raised {@individual.compare_fitness_values(1, 2)}
+				assert_nothing_raised {my_clone.compare_fitness_values(1, 2)}
 			end
-
 		end
-
 	end
-	
 end
